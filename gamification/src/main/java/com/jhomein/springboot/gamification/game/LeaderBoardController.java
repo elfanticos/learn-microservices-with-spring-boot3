d7eccc242cbc079b7c4241920f90702caed13d42
jmeza <jhonatan.meza@gruppogaspari.it>
2024-06-28 22:54:12 +0200

    Add loadbalancer

--- gamification/src/main/java/com/jhomein/springboot/gamification/game/LeaderBoardController.java
+++ gamification/src/main/java/com/jhomein/springboot/gamification/game/LeaderBoardController.java
@@ -2,12 +2,14 @@ package com.jhomein.springboot.gamification.game;
 
 import com.jhomein.springboot.gamification.game.domain.LeaderBoardRow;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/leaders")
@@ -16,6 +18,7 @@ public class LeaderBoardController {
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 }
