62da0df916ff695a0405362d9cf11e7cd6ab67a5
jmeza <jhonatan@smiledu.com>
2024-07-13 19:59:30 +0200

    add consul part 1

---
+++ consul.sh
@@ -0,0 +1,11 @@
+consul agent -node=learnmicro -dev
+
+#config/defaults/application.yml
+#config/defaults,production/application.yml
+
+#config/multiplication,rabbitmq-production/
+#config/multiplication,production/
+#config/multiplication/
+#config/defaults,rabbitmq-production/
+#config/defaults,production/
+#config/defaults/
